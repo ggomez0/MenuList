ab30eb87003f77aae4d7b78088e584554754e1f1
ggomez0 <gaspargomez2000@outlook.com>
2025-08-17 22:01:15 -0300

    Elimina el botón de descarga del encabezado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,14 +13,7 @@ function Header() {
           </div>
   
           <div className="header-contact">
-            <div className="info-section">
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon" id="DownloadButton" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-                        <path d="M7 11l5 5l5 -5" />
-                        <path d="M12 4l0 12" />
-                      </svg>            
-               </div>
+        
               <span className="factura">Emitimos Factura C</span>
             <a
               href="https://api.whatsapp.com/send?phone=5493876291409"
